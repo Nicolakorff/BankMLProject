f31d0b6e41f3e8d4e34e126da0c1d9c3b6daee66
Nicola Korff <nicolavonkorff@gmail.com>
2024-11-22 09:43:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,30 +16,20 @@ with open('scaler.pkl', 'rb') as scaler_file:
 st.title('Predicción del grupo y probabilidad de adquirir depósitos')
 
 # Entrada de datos del usuario
-month_encoded = st.number_input('Momento del contacto de campaña (meses)', min_value=0)
 balance = st.number_input('Sueldo (euros)', min_value=0)
 age = st.number_input('Edad (años)', min_value=0)
 
 # Crear un DataFrame con las entradas
 user_data = pd.DataFrame({
-    'month': [month_encoded],
     'balance ': [balance],
     'age': [age]
 })
 
-# Estandarizar las entradas (excepto 'month')
-user_data_to_scale = user_data.drop(columns=['month'])  # Excluir 'month' de la estandarización
-user_data_standardized = scaler.transform(user_data_to_scale)
-
-# Combinar 'month' con los datos estandarizados
-user_data_combined = pd.DataFrame(user_data_standardized, columns=user_data_to_scale.columns)
-user_data_combined['month'] = user_data['month'].values  # Añadir de nuevo la columna 'month'
-
 # Predicción del clúster con K-means
-cluster_prediction = kmeans_model.predict(user_data_combined)[0]
+cluster_prediction = kmeans_model.predict(user_data)[0]
 
 # Predicción de la probabilidad con el modelo de regresión logística
-probability_prediction = logistic_model.predict_proba(user_data_combined)[0][1]
+probability_prediction = logistic_model.predict_proba(user_data)[0][1]
 
 # Mostrar las predicciones
 st.write(f"El usuario pertenece al clúster: **{cluster_prediction}**")
