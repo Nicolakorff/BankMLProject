6d5e6121f49bfc6191ed0e6c1229c4641621643a
Nicola Korff <nicolavonkorff@gmail.com>
2024-11-22 09:09:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,7 @@ user_data = pd.DataFrame({
 })
 
 try:
-    # Seleccionar las características utilizadas para entrenar KMeans
-    user_data_kmeans = user_data_combined[['age', 'balance']]  # Ajustar según las características originales
-
-    # Predicción del clúster con K-means
-    cluster_prediction = kmeans_model.predict(user_data_kmeans)[0]
- 
+   
     # Estandarizar las entradas (excepto 'month')
     user_data_to_scale = user_data.drop(columns=['month'])  # Excluir 'month' de la estandarización
     user_data_standardized = scaler.transform(user_data_to_scale)
@@ -43,6 +38,12 @@ try:
     # Combinar 'month' con los datos estandarizados
     user_data_combined = pd.DataFrame(user_data_standardized, columns=user_data_to_scale.columns)
     user_data_combined['month'] = user_data['month'].values  # Añadir de nuevo la columna 'month'
+
+    # Seleccionar las características utilizadas para entrenar KMeans
+    user_data_kmeans = user_data_combined[['age', 'balance']]  # Ajustar según las características originales
+
+    # Predicción del clúster con K-means
+    cluster_prediction = kmeans_model.predict(user_data_kmeans)[0]
 
     # Predicción del clúster con K-means
     cluster_prediction = kmeans_model.predict(user_data_combined)[0]
