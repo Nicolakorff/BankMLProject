ede588c3bf0c0162fc498e0b39160083fa5a1bd8
Nicola Korff <nicolavonkorff@gmail.com>
2024-11-22 08:22:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ with open('scaler.pkl', 'rb') as scaler_file:
 st.title('Predicción del grupo y probabilidad de adquirir depósitos')
 
 # Entrada de datos del usuario
-month_encoded = st.number_input('Momento del contacto de campaña (meses)', min_value=0, max_value=11, step=1)
-balance = st.number_input('Balance (euros)', min_value=-5000.0, max_value=100000.0, step=100.0)
+month_encoded = st.number_input('Momento del contacto de campaña (meses)', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 age = st.number_input('Edad (años)', min_value=18, max_value=100, step=1)
+balance = st.number_input('Balance (euros)', min_value=-5000.0, max_value=100000.0, step=100.0)
 campaign = st.number_input('Número de campañas en las que ha sido contactado', min_value=1, max_value=50, step=1)
 
 # Crear un DataFrame con las entradas
 user_data = pd.DataFrame({
     'month': [month_encoded],  # Ajusta los nombres de columnas según tu modelo
-    'balance': [balance],
     'age': [age],
+    'balance': [balance],
     'campaign': [campaign]
 })
 
